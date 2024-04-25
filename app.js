931f7d815e3e824da5a1fd9c2557c8bb469c03a4
zwennnnn <140218730+zwennnnn@users.noreply.github.com>
2024-04-26 01:28:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+async function videoApi() {
+  const domainGirisi = document.getElementById('domainInput').value;
+  const url =
+    'https://domain-da-pa-check.p.rapidapi.com/?target=' +
+    encodeURIComponent(domainGirisi);
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '33e9f20db0mshf6049e02fd97946p1d6d36jsne6cf8ce763a6',
+      'X-RapidAPI-Host': 'domain-da-pa-check.p.rapidapi.com',
+    },
+  };
+  const response = await fetch(url, options);
+  const data = await response.json();
+  const donenRes = document.getElementById('donenRes');
+  const totalResult = document.getElementById('totalResults');
+  const target = document.getElementById('targetDomain');
+  const domainScore = document.getElementById('domainAuth');
+  const pageScore = document.getElementById('pageAuth');
+  const spamScore = document.getElementById('spamScore');
+  const backLinks = document.getElementById('backLinks');
+  const butunResult = JSON.stringify(data, null, 2);
+
+  try {
+    response;
+    data;
+    totalResult.textContent = butunResult;
+    target.textContent = data.body.target;
+    domainScore.textContent = data.body.da_score;
+    pageScore.textContent = data.body.pa_score;
+    spamScore.textContent = data.body.spam_score;
+    backLinks.textContent = data.body.total_backlinks;
+    donenRes.textContent = data.result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+document.getElementById('submitButton').addEventListener('click',videoApi);
